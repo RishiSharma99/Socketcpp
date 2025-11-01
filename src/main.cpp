6a57a3bb08c37b0b2375e02b13986ddb07f22831
Rishi Sharma <rishi17260@iitd.ac.in>
2025-11-01 18:48:53 +0000

    Done with basic TCP Server

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-#include "socket.h"
+#include <cstddef>
 #include <print>
+#include <print>
+#include <sys/socket.h>
 
-import addm;
+
 import socketcpp;
 
 
 int main()
 {
-    net::Socket s{3};
-    std::println("Out : {}", add1(3));
+    auto ip_addr = socketcpp::IpAddress::v4_any(3456).value();
+    auto server_socket = socketcpp::TcpListener::create(
+        ip_addr).value();
+
+    std::println("Listening on : {}:{}", ip_addr.address_string(), ip_addr.port());
+    auto [client_socket, addrs] = server_socket.accept().value();
+
+    std::println("Connection from: {}:{}", addrs.address_string(), addrs.port());
+    std::byte buffer[1024]{};
+
+    auto res = client_socket.recv(buffer).value();
+    
+    auto recv_data = std::string_view(reinterpret_cast<char *>(buffer), res);
+    std::println("Received : {}", recv_data);
+    
 }
